90f380bc56f4a49a3e88592bf23eb2e058851503
Sandeep Somasekharan <codereverser@gmail.com>
2020-12-21 12:47:33 +0530

    Support for parsing folios without advisor

--- casparser/regex.py
+++ casparser/regex.py
@@ -5,7 +5,7 @@ FOLIO_RE = (
     r"KYC\s*:\s*(.+?)\s+PAN\s*:\s*(.+?)$"
 )
 
-SCHEME_RE = r"([\s\w]+)-\s*\d*\s*(.+?)\s*\(Advisor\s*:\s*(.+?)\)\s+Registrar\s*:\s*(.*)\s*$"
+SCHEME_RE = r"([\s\w]+)-\s*\d*\s*(.+?)\s*(?:\(Advisor\s*:\s*(.+?)\))*\s+Registrar\s*:\s*(.*)\s*$"
 OPEN_UNITS_RE = r"Opening\s+Unit\s+Balance.+?([\d,.]+)"
 CLOSE_UNITS_RE = r"Closing\s+Unit\s+Balance.+?([\d,.]+)"
 VALUATION_RE = r"Valuation\s+on\s+(\d{2}-[A-Za-z]{3}-\d{4})\s*:\s*INR\s*([\d,.]+)"
