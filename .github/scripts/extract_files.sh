2aaf6a4937321465ee318ab0150d6da7c8be83d0
Sandeep Somasekharan <codereverser@gmail.com>
2020-12-21 23:44:48 +0530

    fix typo

--- .github/scripts/extract_files.sh
+++ .github/scripts/extract_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 gpg -q --batch --yes -d --passphrase="$FILES_PASSPHRASE" \
-    -o tests/files.tar test/files.enc
+    -o tests/files.tar tests/files.enc
 tar xf tests/files.tar -C tests/
