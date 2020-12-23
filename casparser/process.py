cca89194e4b97ba05d96f5867c2b310367a4c6ed
Sandeep Somasekharan <codereverser@gmail.com>
2020-12-24 02:01:21 +0530

    Support for parsing dividend payout transactions

--- casparser/process.py
+++ casparser/process.py
@@ -5,7 +5,8 @@ from dateutil import parser as date_parser
 
 from .exceptions import HeaderParseError, CASParseError
 from .regex import FOLIO_RE, HEADER_RE, SCHEME_RE
-from .regex import CLOSE_UNITS_RE, NAV_RE, OPEN_UNITS_RE, VALUATION_RE, TRANSACTION_RE
+from .regex import CLOSE_UNITS_RE, NAV_RE, OPEN_UNITS_RE, VALUATION_RE
+from .regex import DESCRIPTION_TAIL_RE, TRANSACTION_RE
 
 
 def parse_header(text):
@@ -31,8 +32,14 @@ def process_cas_text(text):
     current_folio = None
     current_amc = None
     curr_scheme_data = {}
+    balance = Decimal(0.0)
     lines = text.split("\u2029")
     for line in lines:
+        if m := re.search(DESCRIPTION_TAIL_RE, line, re.I | re.DOTALL):
+            description_tail = m.group(1).rstrip()
+            line = line.replace(description_tail, "")
+        else:
+            description_tail = ""
         if amc_match := re.search(r"^(.+?)\s+(MF|Mutual\s+Fund)$", line, re.I | re.DOTALL):
             current_amc = amc_match.group(0)
         elif m := re.search(FOLIO_RE, line, re.I):
@@ -70,6 +77,7 @@ def process_cas_text(text):
                     "valuation": {"date": None, "value": 0, "nav": 0},
                     "transactions": [],
                 }
+                balance = Decimal(0.0)
         if not curr_scheme_data:
             continue
         if m := re.search(OPEN_UNITS_RE, line):
@@ -91,10 +99,15 @@ def process_cas_text(text):
         if m := re.search(TRANSACTION_RE, line, re.DOTALL):
             date = date_parser.parse(m.group(1)).date()
             amt = Decimal(m.group(3).replace(",", "_").replace("(", "-"))
-            units = Decimal(m.group(4).replace(",", "_").replace("(", "-"))
-            nav = Decimal(m.group(5).replace(",", "_"))
-            balance = Decimal(m.group(6).replace(",", "_"))
-            desc = m.group(2).strip()
+            is_dividend = m.group(4) is None
+            if is_dividend:
+                units = None
+                nav = None
+            else:
+                units = Decimal(m.group(4).replace(",", "_").replace("(", "-"))
+                nav = Decimal(m.group(5).replace(",", "_"))
+                balance = Decimal(m.group(6).replace(",", "_"))
+            desc = m.group(2).strip() + description_tail
             curr_scheme_data["transactions"].append(
                 {
                     "date": date,
@@ -103,6 +116,7 @@ def process_cas_text(text):
                     "units": units,
                     "nav": nav,
                     "balance": balance,
+                    "is_dividend": is_dividend,
                 }
             )
     if curr_scheme_data:
