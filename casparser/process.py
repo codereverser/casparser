ecfae47e2b9129bccc8640c179ad553194b5cd43
Sandeep Somasekharan <codereverser@gmail.com>
2020-12-28 20:02:10 +0530

    fixed folio regex search

--- casparser/process.py
+++ casparser/process.py
@@ -42,7 +42,7 @@ def process_cas_text(text):
             description_tail = ""
         if amc_match := re.search(r"^(.+?)\s+(MF|Mutual\s+Fund)$", line, re.I | re.DOTALL):
             current_amc = amc_match.group(0)
-        elif m := re.search(FOLIO_RE, line, re.I):
+        elif m := re.search(FOLIO_RE, line, re.I | re.DOTALL):
             folio = m.group(1).strip()
             if current_folio is None or current_folio != folio:
                 if curr_scheme_data and current_folio is not None:
