e66f139e4b06ec0823a499a3bf6502d0a8045d5f
Sandeep Somasekharan <codereverser@gmail.com>
2020-12-28 11:03:33 +0530

    improved folio regex

--- casparser/regex.py
+++ casparser/regex.py
@@ -1,8 +1,8 @@
 HEADER_RE = r"(?P<from>\d{2}-[a-zA-Z]{3}-\d{4})\s+to\s+(?P<to>\d{2}-[a-zA-Z]{3}-\d{4})"
 
 FOLIO_RE = (
-    r"Folio\s+No\s*:\s+(.+?)\s+(?:PAN\s*:\s+([A-Z]{5}\d{4}[A-Z])\s+)?"
-    r"KYC\s*:\s*(OK|NOT\s+OK)\s*(?:PAN\s*:\s*(OK|NOT\s+OK))?$"
+    r"Folio\s+No\s*:\s+([\d/\s]+)\s+.*?(?:PAN\s*:\s+([A-Z]{5}\d{4}[A-Z])\s+)?.*?"
+    r"KYC\s*:\s*(OK|NOT\s+OK)\s*.*?(?:PAN\s*:\s*(OK|NOT\s+OK))?$"
 )
 
 SCHEME_RE = r"([\s\w]+)-\s*\d*\s*(.+?)\s*(?:\(Advisor\s*:\s*(.+?)\))*\s+Registrar\s*:\s*(.*)\s*$"
