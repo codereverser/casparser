db00cbf342c55fdc6b965df859cfa3e9dae7ce37
Sandeep Somasekharan <codereverser@gmail.com>
2021-04-03 03:56:00 +0530

    support for sorting transactions and fixing balances

--- casparser/parsers/__init__.py
+++ casparser/parsers/__init__.py
@@ -2,16 +2,24 @@ import io
 from typing import Union
 
 from casparser.process import process_cas_text
+from ..types import FolioType
 from .utils import cas2json, cas2csv
 
 
-def read_cas_pdf(filename: Union[str, io.IOBase], password, output="dict", force_pdfminer=False):
+def read_cas_pdf(
+    filename: Union[str, io.IOBase],
+    password,
+    output="dict",
+    sort_transactions=False,
+    force_pdfminer=False,
+):
     """
     Parse CAS pdf and returns line data.
 
     :param filename: CAS pdf file (CAMS or Kfintech)
     :param password: CAS pdf password
     :param output: Output format (json,dict)  [default: dict]
+    :param sort_transactions: Sort transactions by date and re-compute balances.
     :param force_pdfminer: Force pdfminer parser even if mupdf is detected
     """
     if force_pdfminer:
@@ -25,6 +33,22 @@ def read_cas_pdf(filename: Union[str, io.IOBase], password, output="dict", force
     partial_cas_data = cas_pdf_to_text(filename, password)
 
     processed_data = process_cas_text("\u2029".join(partial_cas_data.lines))
+
+    if sort_transactions:
+        folio: FolioType
+        for folio in processed_data.get("folios"):
+            for scheme in folio["schemes"]:
+                dates = [x["date"] for x in scheme["transactions"]]
+                sorted_dates = list(sorted(dates))
+                if dates != sorted_dates:
+                    sorted_transactions = []
+                    balance = scheme["open"]
+                    for transaction in sorted(scheme["transactions"], key=lambda x: x["date"]):
+                        balance += transaction["units"] or 0
+                        transaction["balance"] = balance
+                        sorted_transactions.append(transaction)
+                    scheme["transactions"] = sorted_transactions
+
     # noinspection PyProtectedMember
     processed_data.update(
         {
