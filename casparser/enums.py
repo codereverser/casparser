62ac8e1377fe2cea34f2232cf4046d6da538a99a
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2021-06-26 17:13:45 +0530

    Make TransactionType a str enum instead of int

--- casparser/enums.py
+++ casparser/enums.py
@@ -17,7 +17,15 @@ class CASFileType(IntEnum):
     DETAILED = 2
 
 
-class TransactionType(Enum):
+class TransactionType(str, Enum):
+    # noinspection PyMethodParameters
+    def _generate_next_value_(name, start, count, last_values) -> str:  # type: ignore
+        """
+        Uses the name as the automatic value, rather than an integer
+        See https://docs.python.org/3/library/enum.html#using-automatic-values for reference
+        """
+        return name
+
     PURCHASE = auto()
     PURCHASE_SIP = auto()
     REDEMPTION = auto()
