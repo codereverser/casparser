a641f32bb3a6b8e8f1ab1921e3532915a2752c19
Sandeep Somasekharan <codereverser@gmail.com>
2023-09-10 10:28:43 +0530

    update test cases

--- .github/scripts/extract_files.sh
+++ .github/scripts/extract_files.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 gpg -q --batch --yes -d --passphrase="$FILES_PASSPHRASE" \
-    -o tests/files.tar tests/files.enc
-tar xf tests/files.tar -C tests/
+    -o tests/files.tar.bz2 tests/files.enc
+tar xjf tests/files.tar.bz2 -C tests/
