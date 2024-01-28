f5503c8edc1c0688435037400c908ce106b6c459
Isaac Philip <4974658+isaac-philip@users.noreply.github.com>
2024-01-28 23:29:05 +0530

    additional test-case

--- tests/casparser/test_cli.py
+++ tests/casparser/test_cli.py
@@ -4,3 +4,4 @@ from casparser.cli import format_number
 def test_format_number():
     assert format_number(100) == "100"
     assert format_number(1000) == "1,000"
+    assert format_number(102312) == "102,312"
